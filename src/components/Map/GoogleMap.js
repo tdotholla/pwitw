d22a95ebed27e763301a2c19dd61328cabe46186
tdotholla <kiel.byrne@gmail.com>
2019-04-04 16:04:26 -0400

    added cluster and images

--- src/components/Map/GoogleMap.js
+++ src/components/Map/GoogleMap.js
@@ -1,216 +1,315 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { GoogleMap, LoadScript, MarkerClusterer, Marker  } from '@react-google-maps/api'
+import { GoogleMap, InfoWindow, LoadScript, Marker, MarkerClusterer } from '@react-google-maps/api'
 import { connect } from "react-redux";
-
+import m1 from "./images/m1.png";
+import m2 from "./images/m2.png";
+import m3 from "./images/m3.png";
+import m4 from "./images/m4.png";
+import m5 from "./images/m5.png";
 import * as ACTIONS from "./../../actions/actionConstants";
 // import { getAllWorkstations } from "./../../actions/actionConstants"
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const MapOptions = {
-  clickableIcons: false,
-  disableDefaultUI: true,
-  fullscreenControl: false,  
-  mapTypeControl: false,
-  scaleControl: false,
-  rotateControl: true,
-  streetViewControl: false,
-  gestureHandling: "cooperative",
-  scrollwheel: true,
-  // Map styles; snippets from 'Snazzy Maps'.
-  styles: [
-      {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 17
-              }
-          ]
-      },
-      {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 30
-              }
-          ]
-      },
-      {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000"
-              },
-              {
-                  "lightness": 10
-              }
-          ]
-      },{
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 17
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 29
-              },
-              {
-                  "weight": 0.2
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 18
-              }
-          ]
-      },
-      {
-          "featureType": "road.local",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 16
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#fff"
-              },
-              {
-                  "lightness": 21
-              }
-          ]
-      },
-      {
-          "elementType": "labels.text.stroke",
-          "stylers": [
-              {
-                  "visibility": "on"
-              },
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 25
-              }
-          ]
-      },
-      {
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "saturation": 36
-              },
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 50
-              }
-          ]
-      },
-      {
-          "elementType": "labels.icon",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "elementType": "geometry",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 19
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 20
-              }
-          ]
-      },
-      {
-          "featureType": "administrative",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#000000"
-              },
-              {
-                  "lightness": 17
-              },
-              {
-                  "weight": 1.2
-              }
-          ]
-      }]
-}
+const clusterStyles= [{
+      url: m1,
+      height: 53,
+      width: 53,
+      anchor: [26, 26],
+      textColor: '#000',
+      textSize: 11
+  }, {
+      url: m2,
+      height: 56,
+      width: 56,
+      anchor: [28, 28],
+      textColor: '#000',
+      textSize: 11
+  }, {
+      url: m3,
+      height: 66,
+      width: 66,
+      anchor: [33, 33],
+      textColor: '#000',
+      textSize: 11
+  }, {
+      url: m4,
+      height: 78,
+      width: 78,
+      anchor: [39, 39],
+      textColor: '#000',
+      textSize: 11
+  }, {
+      url: m5,
+      height: 90,
+      width: 90,
+      anchor: [45, 45],
+      textColor: '#000',
+      textSize: 11
+}]
+
+const StationWindow = ({...props}) => {
+  let {stations, position} = props;
+  return stations.map(station => (<InfoWindow
+        position={position}
+        options={{
+          pixelOffset: {height:-30,width:0}
+        }}
+        style={{padding:0}}
+        visible={!!station._id}
+        >
+        <div className="App-infowindow">
+          <h3>{station.hostname}</h3>
+          <p>{station.ip_local}<br />
+          {station.username}<br />
+          {station._changed}</p>
+        </div>
+      </InfoWindow>
+      ))
+  }
 
 class FullMap extends Component {
+  state = {
+    showInfoWindow: null,
+    browserLocation: false,
+    data: {}
+  };
 
   static defaultProps = {
     center: {
       "lat": 38.9065495,
       "lng": -77.0518192
     },
-    zoom: 5
+    zoom: 5,
+    options: {
+      clickableIcons: false,
+      disableDefaultUI: true,
+      fullscreenControl: false,  
+      mapTypeControl: false,
+      scaleControl: false,
+      rotateControl: true,
+      streetViewControl: false,
+      gestureHandling: "cooperative",
+      scrollwheel: true,
+      // Map styles; snippets from 'Snazzy Maps'.
+      styles: [
+          {
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape.natural",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 30
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape.man_made",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000"
+                  },
+                  {
+                      "lightness": 10
+                  }
+              ]
+          },{
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 29
+                  },
+                  {
+                      "weight": 0.2
+                  }
+              ]
+          },
+          {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 18
+                  }
+              ]
+          },
+          {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 16
+                  }
+              ]
+          },
+          {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#fff"
+                  },
+                  {
+                      "lightness": 21
+                  }
+              ]
+          },
+          {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  },
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 25
+                  }
+              ]
+          },
+          {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                  {
+                      "saturation": 36
+                  },
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 50
+                  }
+              ]
+          },
+          {
+              "elementType": "labels.icon",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "transit",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 19
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 20
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  },
+                  {
+                      "weight": 1.2
+                  }
+              ]
+          }]
+    }
   };
 
   componentDidMount() {
+    //calls from db and stores in state.stations
     this.props.getAllWorkstations();
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const coords = pos.coords;
+        this.setState({
+          currentLocation: {
+            lat: coords.latitude,
+            lng: coords.longitude
+          }
+        })
+      })
+    }
   }
+
+  handleMouseOverMarker = (e, data) => {
+    console.log(e, data)
+    this.setState({
+      showInfoWindow: true,
+      data: data
+    })
+  }
+  handleMouseOverCluster = (cluster) => {
+    this.setState({
+      showInfoWindows: true
+    })
+  }
+  handleMouseExitMarker = (e) => {
+    this.setState({
+      showInfoWindow: null
+    })
+  }
+
   render() {
-    const {props} = this;
+    const {center, zoom, options, state} = this.props;
+    const {showInfoWindow} = this.state;
+    const {handleMouseExitMarker, handleMouseOverMarker, handleMouseOverCluster} = this;
     return (
       // Important! Always set the container height explicitly
+      //set via app-map classname
       <LoadScript
         id="script-loader"
         googleMapsApiKey="AIzaSyCa7QqTqLMM66gRtaW9KPFPdfkXgFAG8pA"
@@ -219,38 +318,42 @@ class FullMap extends Component {
       >
         <GoogleMap
           mapContainerClassName="App-map"
-          center={props.center}
-          zoom={props.zoom}
-          options={MapOptions}
-        >
-        
-          {
-// <MarkerClusterer imagePath="https://www.respectfulrevolution.org/sites/all/themes/curiouser_respect/graphics/key4.png">
-            // (clusterer) => 
-            ( props.state.stations.all ) && props.state.stations.all.map(function(ws) {
-              let loc = ws.location.split(',');
-              let locObj = {lat: parseFloat(loc[0]), lng: parseFloat(loc[1])}
-              let image ={
-                url: "https://cdn0.iconfinder.com/data/icons/gloss-basic-icons-by-momentum/32/pin-red.png",
-                
+          center={center}
+          zoom={zoom}
+          options={options}
+        >{
+          state.stations.all && (<MarkerClusterer 
+            imagePath={`${process.env.PUBLIC_URL}/images/m`} 
+            styles={clusterStyles}
+            onMouseOver={handleMouseOverCluster} >{
+              // showInfoWindow && (<StationWindow position={this.position} stations={this.stations}/>)}
+              // {
+                (clusterer) => { 
+                return state.stations.all.map((ws) => {
+                  let loc = ws.location.split(',');
+                  let locObj = {lat: parseFloat(loc[0]), lng: parseFloat(loc[1])}
+                  let image ={
+                    url: "https://cdn0.iconfinder.com/data/icons/gloss-basic-icons-by-momentum/32/pin-red.png",
+                  }
+                  return (
+                    (ws.location) && (
+                      <Marker
+                        className="App-marker"
+                        key={ws._id}
+                        position={locObj}
+                        clusterer={clusterer}
+                        icon={image}
+                        title={`${ws.hostname}-${ws.username}`}
+                        onMouseOver={(e) => handleMouseOverMarker(e, ws)}
+                        onMouseOut={handleMouseExitMarker}
+                      >
+                      </Marker>
+                      )
+                    )
+                  })
               }
-              return (
-                (ws.location) && (
-                  <Marker
-                    className="App-marker"
-                    key={ws._id}
-                    position={locObj}
-                    // clusterer={clusterer}
-                    icon={image}
-                    title={`${ws.hostname}-${ws.username}`}
-                  />
-                )
-              )
-            })
-        // </MarkerClusterer>
-        }
-        
-        </GoogleMap>
+              }</MarkerClusterer> )
+            }</GoogleMap>
       </LoadScript>
     );
   }
