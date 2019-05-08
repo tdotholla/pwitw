6cd929b1cacf51ca45dfa7aa1a22748f7de50bb1
Byrne <byrne_admin@perkinswill.com>
2019-05-08 00:09:56 -0400

    added sortable material-ui table

--- src/components/Map/StatsList.js
+++ src/components/Map/StatsList.js
@@ -11,20 +11,69 @@ import Divider from '@material-ui/core/Divider';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-
 import ListItem from '@material-ui/core/ListItem';
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import TableSortLabel from '@material-ui/core/TableSortLabel'
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExploreIcon from '@material-ui/icons/Explore';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-import { isHome } from "../../functions";
+import { lighten } from '@material-ui/core/styles/colorManipulator'
+
+import { isHome, desc, getSorting, stableSort } from "../../functions";
 // import * as ACTIONS from "./../../actions/actionConstants";
 
 const drawerWidth = 240;
 
+const toolbarStyles = theme => ({
+  root: {
+    paddingRight: theme.spacing.unit,
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  actions: {
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+});
+
 const styles = theme => ({
   root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
     display: 'flex',
+  },
+  table: {
+
+  },
+  tableWrapper: {
+    overflowX: 'auto'
   },
   hide: {
     display: 'none',
@@ -71,7 +120,105 @@ const styles = theme => ({
   }
 });
 
-class StatsList extends React.Component {
+const rows = [
+  { id: '_changed', numeric: false, disablePadding: true, label: 'Last Logon (approx.)' },
+  { id: 'hostname', numeric: false, disablePadding: true, label: 'Hostname' },
+  { id: 'ip_local', numeric: false, disablePadding: true, label: 'Location (approx.)' },
+  { id: 'username', numeric: false, disablePadding: true, label: 'UserName' },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={numSelected === rowCount}
+              onChange={onSelectAllClick}
+            />
+          </TableCell>
+          {rows.map(
+            row => (
+              <TableCell
+                key={row.id}
+                align={row.numeric ? 'right' : 'left'}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            ),
+            this,
+          )}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+let EnhancedTableToolbar = props => {
+  const { numSelected, classes } = props;
+
+  return (
+    <Toolbar
+      className={classNames(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      <div className={classes.title}>
+        {numSelected > 0 ? (
+          <Typography color="inherit" variant="subtitle1">
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography variant="h6" id="tableTitle">
+            Asset Tracking
+          </Typography>
+        )}
+      </div>
+      <div className={classes.spacer} />
+      <div className={classes.actions}>
+        {numSelected > 0 ? (
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton aria-label="Filter list">
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </div>
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
+
+class StatsTable extends React.Component {
 	static propTypes = {
 		data: PropTypes.array,
 	};
@@ -80,6 +227,11 @@ class StatsList extends React.Component {
 		super(props);
 		this.state = {
 	    open: false,
+      order: 'desc',
+      orderBy: 'hostname',
+      selected: [],
+      page: 0,
+      rowsPerPage: 10
 	  };
 	}
 
@@ -91,18 +243,76 @@ class StatsList extends React.Component {
     this.setState({ open: false });
   };
 
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event,property)
+  };
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleSelectAllClick = event => {
+    if (event.target.checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }));
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
 	render() {
-    const { classes, data } = this.props;
+    const { classes, data} = this.props;
+    const {order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     let sortable_by_logon = []
     data.map((ws) => {
       return sortable_by_logon.push( [ws.hostname, new Date(ws._changed), ws.ip_local] )
     })
     sortable_by_logon = sortable_by_logon.sort((a,b) => b[1]- a[1])
     const not_in_office = Object.values(data).filter(val => val['ip_local'].split('.')[1] !== '117' )
+    
     const headers = ["Last Logon", "Hostname", "Location"]
 		return (
-			<div>
-        <Drawer
+      <Paper className={classes.root}>
+      <Drawer
           variant="permanent"
           className={classNames(classes.drawer, {
             [classes.drawerOpen]: this.state.open,
@@ -116,60 +326,71 @@ class StatsList extends React.Component {
           }}
           open={this.state.open}
         >
-          <div className={classes.toolbar}>
-          </div>
-          <Divider />
-          <List>
-              <ListItem disableGutters>
-                <ExpansionPanel>
-                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}><ScheduleIcon fontSize="small"/> STATS </Typography>
-                  </ExpansionPanelSummary>
-                  <ExpansionPanelDetails>
-                    <table >
-                      <tbody>
-                        <tr>{headers.map((title)=> <th key={title}>{title}</th> )}
-                        </tr>
-                        {sortable_by_logon.map((ws) => (
-                          <tr key={ws[0]}>
-                            <td>{distanceInWords(new Date(ws[1]), new Date())}</td>
-                            <td>{ws[0]}</td>
-                            <td>{isHome(ws[2]) ? <span className={classes.safe}>LAN</span> : <span className={classes.warning}>WAN</span>}</td>
-                          </tr>
-                          )
-                        )}
-                      </tbody>
-                    </table>
-                  </ExpansionPanelDetails>
-                </ExpansionPanel>
-              </ListItem>
-                        <Divider />
-{/*
-              <ListItem disableGutters>
-                <ExpansionPanel>
-                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}><ExploreIcon fontSize="small"/> Outside of LAN </Typography>
-                  </ExpansionPanelSummary>
-                  <ExpansionPanelDetails>
-                  <table>
-                    <tbody>
-                      <tr><th>Hostname</th><th>Public IP</th></tr>
-                      {not_in_office.map((ws) => (
-                        <tr key={ws.hostname}><td>{ws.hostname}</td><td>{ws.ip_public}</td></tr>
-                        )
-                      )}
-                    </tbody>
-                  </table>
-                  </ExpansionPanelDetails>
-                </ExpansionPanel>
-              </ListItem>
-*/
-}          </List>
-          <Divider />
-        </Drawer>
-			</div>
-		);
+        <EnhancedTableToolbar numSelected={selected.length} />
+
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={this.handleSelectAllClick}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(n => {
+                  const isSelected = this.isSelected(n.id);
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      role="checkbox"
+                      aria-checked={isSelected}
+                      tabIndex={-1}
+                      key={n.hostname}
+                      selected={isSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox checked={isSelected} />
+                      </TableCell>
+                      <TableCell component="th" scope="row" padding="none">{distanceInWords(new Date(n._changed), new Date())}</TableCell>
+                      <TableCell align="right">{n.hostname}</TableCell>
+                      <TableCell align="right">{isHome(n.ip_local) ? <span className={classes.safe}>LAN</span> : <span className={classes.warning}>WAN</span>}</TableCell>
+                      <TableCell align="center">{n.username}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+      </Drawer>
+      </Paper>
+    );
 	}
 }
 
-export default withStyles(styles, { withTheme: true })(StatsList);
+export default withStyles(styles, { withTheme: true })(StatsTable);
--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,33 @@
 
 
 export const isHome = (ip) => {
-  const home_octets = ["117", "126", "112", "124"]
+  const home_octets = ["117", "126", "112", "124", "108", "101", "103", "111", "109", "126", "150"]
+  let base = ip.split('.')[0]
   let octet = ip.split('.')[1]
-  if (home_octets.includes(octet)) {return true}
+  if (base === "10" && home_octets.includes(octet)) {return true}
   return false
 }
 
+export function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+export function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+export function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
